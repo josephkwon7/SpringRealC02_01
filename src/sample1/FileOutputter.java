745e2c7819b748aa1a8e7249bb2664f7114edb9c
josephkwon <devel.kwon@gmail.com>
2018-10-12 14:36:44 +0900

    initial commit

---
+++ src/sample1/FileOutputter.java
@@ -0,0 +1,19 @@
+package sample1;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileOutputter implements Outputter {
+    
+    private String filePath;
+
+    public void output(String message) throws IOException {
+        FileWriter out = new FileWriter(filePath);
+        out.write(message);
+        out.close();
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
